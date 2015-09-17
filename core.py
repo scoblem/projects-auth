9777e923e6afcd54c7af1b5ca245658d98b97101
Mark Scoble <scoble.mr@gmail.com>
2015-09-18 00:53:10 +1000

    Initial Upload

---
+++ core.py
@@ -0,0 +1,19 @@
+import json
+
+SECRET_KEY = 'm9rd8iau9b1-o8v=e3h0p&qcjn$t(*+e!b8(df0-4fu#1s(xz3'
+
+def get_auth():
+    try:
+        with open('database.json', 'r') as f:
+            in_data = json.load(f)
+    except (IOError, ValueError):
+        in_data = {}
+    return in_data
+
+# overwrite json file with current state of 'database'.
+def save_auth(out_data):
+    with open('database.json', 'w') as f:
+        json.dump(out_data, f)
+
+
+# {"admin": ["superuser", "123"]}
---
+++ passwd.py
@@ -0,0 +1,150 @@
+import core, bcrypt, sys
+from getpass import getpass
+
+class UserClass(object):
+    database = core.get_auth()
+    key = core.SECRET_KEY
+
+    def __init__(self, user, password):
+        self.user = user
+        self.password = password
+
+    # If user is valid check password is correct.
+    def user_auth(self):
+        return bcrypt.hashpw(self.password, self.database[self.user][0])
+
+    # If user_auth returns True check if user is in group 'superuser'.
+    def admin_auth(self):
+        if self.user_auth():
+            return 'superuser' in self.database[self.user][1]
+        else:
+            return False
+
+    def add_user(self, new_user, new_password, group):
+        hashed_password = bcrypt.hashpw(new_password+core.SECRET_KEY, bcrypt.gensalt())
+        self.database[new_user] = ['0', ['1']]
+        self.database[new_user][0] = hashed_password
+        self.database[new_user][1][0] = group
+        core.save_auth(self.database)
+
+    def del_user(self, select_user):
+        del self.database[select_user]
+        core.save_auth(self.database)
+
+    def reset_password(self, select_user, new_password):
+        hashed_password = bcrypt.hashpw(new_password+core.SECRET_KEY, bcrypt.gensalt())
+        self.database[select_user][0] = hashed_password
+        core.save_auth(self.database)
+
+    def add_group(self, select_user, group):
+        self.database[select_user][1].append(group)
+        core.save_auth(self.database)
+
+    def remove_group(self, select_user, group):
+        self.database[select_user][1].remove(group)
+        core.save_auth(self.database)
+
+class HandlerClass(UserClass):
+
+    def __init__(self, user, password):
+        UserClass.__init__(self, user, password)
+        self.dispatch = {
+                'exit': sys.exit,
+                'add-user': self.add_user_handler,
+                'remove-user': self.del_user_handler,
+                'reset-password': self.reset_password_handler,
+                'add-group': self.add_group_handler,
+                'remove-group': self.remove_group_handler
+                }
+
+    def add_user_handler(self):
+        new_user = input("New User: ").lower()
+        if new_user not in self.database:
+            group = input("User Group (default is [default-group]: ").lower() or "default-group"
+            new_password = getpass('Enter New Password: ')
+            confirm_password = getpass('Confirm New Password: ')
+            if new_password == confirm_password:
+                self.add_user(new_user, new_password, group)
+                print('New User Registered')
+                loop()
+            else:
+            # raise ValueError('User already exists.')
+                print('Passwords do not match.')
+                loop()
+        else:
+            print('User already exists.')
+            # print(spacer)
+            loop()
+
+    def del_user_handler(self):
+        select_user = input("Select user: ").lower()
+        if select_user in self.database:
+            self.del_user(select_user)
+            print('User removed.')
+            loop()
+        else:
+        # raise ValueError('User already exists.')
+            print('User does not exsist.')
+            loop()
+
+    def reset_password_handler(self):
+        select_user = input("Select user: ").lower()
+        if select_user in self.database:
+            new_password = getpass('Enter New Password: ')
+            confirm_password = getpass('Confirm New Password: ')
+            if new_password == confirm_password:
+                self.reset_password(select_user, new_password)
+                print("Password reset.")
+                loop()
+            else:
+                print("Password did not match, try again.")
+                loop() # try except here
+        else:
+            print('User does not exist.')
+            loop()
+
+    def add_group_handler(self):
+        select_user = input("Select user: ").lower()
+        if select_user in self.database:
+            group = input('Enter new group: ')
+            self.add_group(select_user, group)
+            print('Group added to {0}'.format(select_user))
+            loop()
+        else:
+            print('User does not exist.')
+            loop()
+
+    def remove_group_handler(self):
+        select_user = input("Select user: ").lower()
+        if select_user in self.database:
+            try:
+                group = input('Enter group: ')
+                self.remove_group(select_user, group)
+                print('{0} removed from {1}'.format(select_user, group))
+                loop()
+            except:
+                print('{0} is not a member of {1}'.format(select_user, group))
+                loop()
+        else:
+            print('User does not exist.')
+
+user = input('Username: ').lower()
+password = getpass('Password: ') + core.SECRET_KEY
+login = HandlerClass(user, password)
+
+def main():
+    if login.admin_auth() == True:
+        print('User Authenticated as {0}'.format(login.user))
+        print('Valid commands are: ')
+        loop()
+    else:
+        print("Access Denied.")
+
+def loop():
+    display_options = (list(login.dispatch.keys()))
+    print(display_options)
+    return login.dispatch[input(': ').lower()]()
+
+
+if __name__ == '__main__':
+    main()
